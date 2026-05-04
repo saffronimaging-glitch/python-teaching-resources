7ecc0f2049f6bc592e3c03cd2f44dae0a55cdc9b
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 10:18:46 +0100

    Implement data type identification script
    Added a script to identify and print data types of variables.

---
+++ 01_data_&_data_types/challenge_1_identify_types.py
@@ -0,0 +1,15 @@
+# Task: Identify the data type of each variable.
+# Use type() to check your answers.
+
+a = 25
+b = 3.14
+c = "Hello"
+d = False
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# Challenge:
+# Create one variable of each type yourself.
