ca2c7f851376650277824f41ca2d04568f9544a2
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 13:01:07 +0100

    Add arithmetic practice script for basic operations

---
+++ 04_operators_&_arithmetic/challenge_1_arithmetic_practice.py
@@ -0,0 +1,11 @@
+# Topic: Arithmetic Challenge
+# Task:
+# Change the numbers below and predict the output before running the code.
+
+number_1 = 12
+number_2 = 4
+
+print("Addition:", number_1 + number_2)
+print("Subtraction:", number_1 - number_2)
+print("Multiplication:", number_1 * number_2)
+print("Division:", number_1 / number_2)
