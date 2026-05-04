be92eb8087d85c0aece46d01e5061b99670804ef
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 12:01:36 +0100

    Example of dictionaries

---
+++ 03_collections_lists_dictionaries/example_3_dictionaries.py
@@ -0,0 +1,17 @@
+# Topic: Dictionaries
+# Description: Dictionaries store data in key-value pairs.
+# Key idea: Values are accessed using keys.
+
+student = {
+    "name": "Aisha",
+    "age": 14,
+    "subject": "Computer Science"
+}
+
+print("Student name:", student["name"])
+print("Student age:", student["age"])
+
+# Add new data
+student["hobby"] = "Gaming"
+
+print("Updated dictionary:", student)
