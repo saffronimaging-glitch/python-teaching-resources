3e8fb37feea8684cac2dc39cdb0272ba5213a360
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 13:24:53 +0100

    Fix arithmetic operation and update topic description
    Updated topic description and fixed arithmetic operation to return the correct answer.

--- 04_operators_&_arithmetic/debug_2.py
+++ 04_operators_&_arithmetic/debug_2.py
@@ -1,10 +1,10 @@
-# Topic: Debugging Arithmetic
+# Topic: Debugging Arithmetic and Division
 # Problem:
-# This code causes an error. Fix it so the answer is 15.
+# This code causes an incorrect output. Fix it so the answer is 2.
 
 number_1 = "10"
-number_2 = 5
+number_2 = "5"
 
-answer = number_1 + number_2
+answer = number_1 % number_2
 
 print(answer)
