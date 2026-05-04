006287b50191b68d46bb5eacaef465a0ecae2d4b
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 14:15:17 +0100

    Example 1 Boolean Values

---
+++ 05_boolean_logic/example_1_boolean_values.py
@@ -0,0 +1,8 @@
+# Topic: Boolean Values
+# Description: Booleans can only be True or False
+
+is_raining = True
+is_sunny = False
+
+print("Is it raining?", is_raining)
+print("Is it sunny?", is_sunny)
