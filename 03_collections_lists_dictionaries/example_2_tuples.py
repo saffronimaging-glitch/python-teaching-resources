490c788877c0fb1614d456c304074d74e0b20001
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 11:59:53 +0100

    Create example for tuples in Python
    Added an example demonstrating tuples in Python.

---
+++ 03_collections_lists_dictionaries/example_2_tuples.py
@@ -0,0 +1,11 @@
+# Topic: Tuples
+# Description: Tuples store multiple items like lists, but cannot be changed.
+# Key idea: Tuples are ordered but immutable - not changable.
+
+coordinates = (10, 20)
+
+print("Coordinates:", coordinates)
+print("X value:", coordinates[0])
+
+# This would cause an error:
+# coordinates[0] = 15
