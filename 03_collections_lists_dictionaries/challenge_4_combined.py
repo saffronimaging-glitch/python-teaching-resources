3a6b5bedaf426559e948cfd6c834ebbc137d8b7b
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 12:04:20 +0100

    Create initial student list and dictionary structure
    Initialized student list and dictionary for student info.

---
+++ 03_collections_lists_dictionaries/challenge_4_combined.py
@@ -0,0 +1,15 @@
+# Topic: Collections Combined
+# Task:
+# 1. Create a list of 3 student names.
+# 2. Create a dictionary for ONE student with:
+#    name and age.
+# 3. Print both.
+
+students = []
+
+student_info = {}
+
+# Your code here
+
+# Stretch:
+# Add another key to the dictionary.
