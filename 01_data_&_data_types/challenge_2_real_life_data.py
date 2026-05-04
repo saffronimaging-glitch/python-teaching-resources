5b31007887047b79b0fe0089049b3eb9e0163913
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 10:19:25 +0100

    Create personal description variables in Python
    Added a script to create and print personal variables.

---
+++ 01_data_&_data_types/challenge_2_real_life_data.py
@@ -0,0 +1,15 @@
+# Task:
+# Create variables to describe a person.
+
+name = "Your name"
+age = 0
+height = 0.0
+is_student = False
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is student:", is_student)
+
+# Challenge:
+# Change the values to describe yourself.
