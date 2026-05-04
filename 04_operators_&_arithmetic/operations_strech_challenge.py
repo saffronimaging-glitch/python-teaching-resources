20b0ad8922198d7cf316cb8913a46cd5b0a2281d
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 13:29:41 +0100

    Create operations_strech_challenge.py for arithmetic tasks
    Added a new Python file for arithmetic stretch challenges with tasks to create specific calculations.

---
+++ 04_operators_&_arithmetic/operations_strech_challenge.py
@@ -0,0 +1,29 @@
+# Topic: Stretch Challenge - Arithmetic
+# Rules:
+# Only use: +  -  *  /  //  %
+
+# Task 1:
+# Create a calculation that equals 24
+answer_1 = 
+
+# Task 2:
+# Create a calculation that equals 3 using integer division
+answer_2 = 
+
+# Task 3:
+# Create a calculation with a remainder of 2
+answer_3 = 
+
+# Task 4:
+# Use brackets to change the result
+answer_4 = 10 + 5 * 2
+answer_5 = (10 + 5) * 2
+
+print(answer_1)
+print(answer_2)
+print(answer_3)
+print(answer_4)
+print(answer_5)
+
+# Final Challenge:
+# Create 3 different calculations that all equal 20
