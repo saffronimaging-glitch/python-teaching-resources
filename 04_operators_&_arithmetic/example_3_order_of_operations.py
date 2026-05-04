45c8ec8fb6db328cfb6a105aad96e2638f1973f9
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 13:00:40 +0100

    Create example for order of operations in Python
    Added example demonstrating order of operations in Python.

---
+++ 04_operators_&_arithmetic/example_3_order_of_operations.py
@@ -0,0 +1,10 @@
+# Topic: Order of Operations
+# Description: Python follows BODMAS / BIDMAS rules.
+
+answer = 10 + 5 * 2
+
+print(answer)
+
+# Python multiplies first:
+# 5 * 2 = 10
+# 10 + 10 = 20
