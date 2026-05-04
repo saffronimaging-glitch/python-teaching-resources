e989957264bc23d4797788ec3a6e3615af39aa5a
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 13:06:48 +0100

    Fix division operator to get whole number answer
    Change division operator to floor division for whole number result.

---
+++ 04_operators_&_arithmetic/debug_3.py
@@ -0,0 +1,11 @@
+# Topic: Debugging Division
+# Problem:
+# The programmer wants the whole number answer only.
+# Fix the operator.
+
+total_sweets = 17
+students = 5
+
+answer = total_sweets / students
+
+print("Each student gets:", answer)
