5855a3c5978a8b173f8fd4abfe4df0f85b9c0287
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 09:42:09 +0100

    Fix addition to output 15 by converting number_1
    Convert number_1 to an integer to ensure correct addition.

---
+++ 02_variables_&_constants/debug_3.py
@@ -0,0 +1,11 @@
+# Topic: Debugging Data Types
+# Problem:
+# This code runs, but the output may surprise you.
+# Fix it so the answer is 15.
+
+number_1 = "10"
+number_2 = 5
+
+answer = number_1 + number_2
+
+print(answer)
