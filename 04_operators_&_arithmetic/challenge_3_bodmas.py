32bf14ab16295b8943ed871014dbdd92f16ef43e
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 13:02:53 +0100

    Create BODMAS challenge
    Added a BODMAS challenge with sample calculations and a prompt for user input.

---
+++ 04_operators_&_arithmetic/challenge_3_bodmas.py
@@ -0,0 +1,14 @@
+# Topic: BODMAS Challenge
+# Task:
+# Predict the answer before running the code.
+
+answer_1 = 10 + 5 * 2
+answer_2 = (10 + 5) * 2
+answer_3 = 20 - 4 / 2
+
+print(answer_1)
+print(answer_2)
+print(answer_3)
+
+# Challenge:
+# Create your own calculation using brackets.
