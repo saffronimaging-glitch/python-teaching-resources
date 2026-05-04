4eca94dee975ce5731e212a7324c4f3f9b2e9ecb
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 14:20:11 +0100

    Implement boolean logic predictions in challenge 3
    Add boolean logic predictions and challenge prompts.

---
+++ 05_boolean_logic/challenge_3_logic_combinations.py
@@ -0,0 +1,11 @@
+# Task:
+# Predict the result before running
+
+print(True and True)
+print(True and False)
+print(False or True)
+print(not False)
+
+# Challenge:
+# Write 2 expressions that return True
+# Write 2 expressions that return False
