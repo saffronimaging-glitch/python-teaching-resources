a25e7a7b75e775f5b59e2fc02bae6ebc603d6410
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 13:01:36 +0100

    Create division practice script with output predictions
    Added a division practice challenge with comments.

---
+++ 04_operators_&_arithmetic/challenge_2_division_practice.py
@@ -0,0 +1,13 @@
+# Topic: Division Challenge
+# Task:
+# Predict the output of each line before running the code.
+
+number_1 = 23
+number_2 = 4
+
+print(number_1 / number_2)
+print(number_1 // number_2)
+print(number_1 % number_2)
+
+# Challenge:
+# Change the numbers so the remainder is 0.
