a6cae60bc49892a4875504357dfb3788ce4e47cb
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 10:21:11 +0100

    Correct speed calculation to use float division
    Fix calculation to ensure speed is a decimal value.

---
+++ 01_data_&_data_types/debug_2_float_vs_integer.py
@@ -0,0 +1,11 @@
+# Topic: Debugging Data Types
+# Problem:
+# The result should be a decimal, but it is not correct.
+# Fix the code.
+
+distance = 5
+time = 2
+
+speed = distance / time
+
+print("Speed:", speed)
