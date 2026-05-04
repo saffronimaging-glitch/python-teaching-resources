a4701b3ebd84345055bc947a9b3cd62c5362f512
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 14:17:40 +0100

    Add real world logic
    Implement real-world logic for cinema entry based on ticket and age.

---
+++ 05_boolean_logic/example_4_real_world_logic.py
@@ -0,0 +1,9 @@
+# Topic: Real-world Boolean Logic
+# Description: Using logic in a real situation
+
+has_ticket = True
+is_old_enough = True
+
+can_enter = has_ticket and is_old_enough
+
+print("Can enter cinema:", can_enter)
