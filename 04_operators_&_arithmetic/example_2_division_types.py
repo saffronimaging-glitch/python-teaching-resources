44bf7902af91319c9993ae2535a45e728d756f73
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 13:00:06 +0100

    Add example for division types in Python
    This script demonstrates different division types in Python.

---
+++ 04_operators_&_arithmetic/example_2_division_types.py
@@ -0,0 +1,9 @@
+# Topic: Division Types
+# Description: Python has different ways to divide numbers.
+
+number_1 = 17
+number_2 = 5
+
+print("Normal division:", number_1 / number_2)
+print("Integer division:", number_1 // number_2)
+print("Remainder:", number_1 % number_2)
