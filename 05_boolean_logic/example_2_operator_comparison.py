5e7d9e76afab654fb209939f58bb14cd2e7c460f
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 14:15:49 +0100

    Add example of comparison operators in Python
    This file demonstrates the use of comparison operators in Python.

---
+++ 05_boolean_logic/example_2_operator_comparison.py
@@ -0,0 +1,7 @@
+# Topic: Comparison Operators
+# Description: These compare values and return True or False
+
+print(5 > 3)     # True
+print(10 == 5)   # False
+print(7 != 3)    # True
+print(4 < 2)     # False
