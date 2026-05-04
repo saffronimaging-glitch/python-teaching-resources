c0aab98b0325272488b780e0cc59b73c3e037fba
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 13:05:33 +0100

    Fix arithmetic error in debug_2.py
    Convert number_1 to an integer to fix the addition.

---
+++ 04_operators_&_arithmetic/debug_2.py
@@ -0,0 +1,10 @@
+# Topic: Debugging Arithmetic
+# Problem:
+# This code causes an error. Fix it so the answer is 15.
+
+number_1 = "10"
+number_2 = 5
+
+answer = number_1 + number_2
+
+print(answer)
