441300c4086ae93293d446810b8cb5438450edc7
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 12:02:05 +0100

    Create and update a list of favorite foods
    Implement a list of favorite foods with print statements.

---
+++ 03_collections_lists_dictionaries/challenge_1_lists.py
@@ -0,0 +1,11 @@
+# Topic: Lists
+# Task:
+# 1. Create a list of 3 favourite foods.
+# 2. Print the list.
+# 3. Add one more item.
+# 4. Print the updated list.
+# 5. Print the second item in the list.
+
+foods = []
+
+# Your code here
