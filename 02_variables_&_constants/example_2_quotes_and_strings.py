cc92495b6e9204e37775f2f8fdf21052b3e3cd98
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 09:29:33 +0100

    Add example of variables and strings in Python

---
+++ 02_variables_&_constants/example_2_quotes_and_strings.py
@@ -0,0 +1,16 @@
+# Topic: Variables and Strings
+# Description: Strings must be placed inside quotation marks.
+# Key idea: Text data uses quotes, but numbers usually do not.
+
+name = "John"
+school = "Primark School"
+year_group = 9
+
+print("My name is", name)
+print("I go to", school)
+print("I am in Year", year_group)
+
+# Quotation marks allow us to store text:
+message = "Python is fun!"
+
+print(message)
