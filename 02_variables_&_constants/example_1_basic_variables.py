50d9bafe39ea02f1ca933248abf32467de30bb49
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 09:24:18 +0100

    Add basic variables example for student info
    This script demonstrates the use of variables to store and display student information.

---
+++ 02_variables_&_constants/example_1_basic_variables.py
@@ -0,0 +1,11 @@
+# Topic: Variables
+# Description: Variables store data so we can use it later.
+# Key idea: A variable name points to a value.
+
+student_name = "Aisha"
+student_age = 14
+favourite_subject = "Computer Science"
+
+print("Student name:", student_name)
+print("Student age:", student_age)
+print("Favourite subject:", favourite_subject)
