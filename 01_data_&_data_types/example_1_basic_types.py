273a7366d21a5437b6522b8498b3f11787293021
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 10:16:45 +0100

    example_1_basic_types.py

---
+++ 01_data_&_data_types/example_1_basic_types.py
@@ -0,0 +1,19 @@
+# Topic: Data Types
+# Description: Python uses different data types to store different kinds of information.
+
+# Integer (whole number)
+age = 14
+
+# Float (decimal number)
+height = 1.65
+
+# String (text inside quotes)
+name = "Aisha"
+
+# Boolean (True or False)
+is_student = True
+
+print("Age:", age)
+print("Height:", height)
+print("Name:", name)
+print("Is a student:", is_student)
