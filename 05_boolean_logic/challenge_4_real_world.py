0d2542c0d77722d1698d70513a026e6c5f5d6b83
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 14:21:22 +0100

    Add homework decision logic
    Implement decision logic for homework based on weekend status.

---
+++ 05_boolean_logic/challenge_4_real_world.py
@@ -0,0 +1,10 @@
+# Task:
+# Create a simple decision using logic
+
+has_homework = True
+is_weekend = False
+
+# Write a condition that prints:
+# "Do homework" only if it is NOT the weekend AND homework exists
+
+# Your code here
