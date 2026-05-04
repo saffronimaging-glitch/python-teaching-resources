3a8985cdeb20144bd825c1f1adfcfde259088bb3
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 12:59:34 +0100

    Basic arithmetic example in Python
    Added an example of basic arithmetic operations in Python.

---
+++ 04_operators_&_arithmetic/example_1_basic_arithmetic.py
@@ -0,0 +1,10 @@
+# Topic: Arithmetic Operators
+# Description: Python can perform basic arithmetic calculations.
+
+number_1 = 20
+number_2 = 5
+
+print("Addition:", number_1 + number_2)
+print("Subtraction:", number_1 - number_2)
+print("Multiplication:", number_1 * number_2)
+print("Division:", number_1 / number_2)
