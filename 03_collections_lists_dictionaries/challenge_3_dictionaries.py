525a16ecc301ff271960c7d6d6f4f99652bce850
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-04 12:03:34 +0100

    Challenge of creating a dictionary
    Create a dictionary to describe a student and include a hobby key.

---
+++ 03_collections_lists_dictionaries/challenge_3_dictionaries.py
@@ -0,0 +1,14 @@
+# Topic: Dictionaries
+# Task:
+# Create a dictionary to describe a student with:
+# - name
+# - age
+# - favourite subject
+
+student = {}
+
+# Your code here
+
+# Challenge:
+# Add a new key called "hobby"
+# Print the hobby value
