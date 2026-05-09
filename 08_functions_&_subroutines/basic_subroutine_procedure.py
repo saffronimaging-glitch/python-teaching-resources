4e378d3e15072b4d51a168bb8816d8facc619e1c
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-09 12:29:28 +0100

    Update and rename def_functions.py to basic_subroutine_procedure.py

---
+++ 08_functions_&_subroutines/basic_subroutine_procedure.py
@@ -0,0 +1,10 @@
+# BASIC SUBROUTINE / PROCEDURE
+# A procedure does something, but does not return a value.
+
+def welcome_message():
+    print("Welcome to the Quiz Game!")
+    print("Answer each question carefully.")
+    print("Good luck!")
+
+
+welcome_message()
