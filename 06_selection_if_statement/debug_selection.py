fb07f4140cc28dd48823a36e26aed7c3eb808d38
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-09 12:00:06 +0100

    Debug selection code with fixed conditions and syntax
    Fixed syntax errors in if statements and corrected indentation.

---
+++ 06_selection_if_statement/debug_selection.py
@@ -0,0 +1,33 @@
+# Debug the Selection Code
+
+# Debug 1
+score = 75
+
+if score > 80:
+    print("Grade A")
+
+elif score > 70:
+print("Grade B")
+
+else:
+    print("Grade C")
+
+
+# Debug 2
+answer = input("What colour is grass? ")
+
+if answer.lower() = "green":
+    print("Correct!")
+
+else:
+    print("Incorrect!")
+
+
+# Debug 3
+logged_in = False
+
+if logged_in == True:
+    print("Access granted")
+
+else
+    print("Access denied")
