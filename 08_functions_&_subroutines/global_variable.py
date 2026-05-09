2fad01830bb27041d9b58799b4d94fc0e5c3f81a
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-09 12:32:23 +0100

    Implement global variable and score display function
    Added a global variable 'score' and a function to display it.

---
+++ 08_functions_&_subroutines/global_variable.py
@@ -0,0 +1,11 @@
+# GLOBAL VARIABLES
+# A global variable is created outside functions and can be seen by the whole program.
+
+score = 0
+
+
+def show_score():
+    print("Current score:", score)
+
+
+show_score()
