7958e39a4b473a6a40598fddcfd3f57eb73513cc
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-09 11:29:50 +0100

    Add logic gate examples in Python

---
+++ 05_boolean_logic/logic_gate_examples.py
@@ -0,0 +1,25 @@
+# Logic Gates in Python
+
+a = True
+b = False
+
+# AND gate
+print("AND:", a and b)
+
+# OR gate
+print("OR:", a or b)
+
+# NOT gate
+print("NOT A:", not a)
+
+# NAND gate
+print("NAND:", not (a and b))
+
+# NOR gate
+print("NOR:", not (a or b))
+
+# XOR gate
+print("XOR:", a != b)
+
+# XNOR gate
+print("XNOR:", a == b)
