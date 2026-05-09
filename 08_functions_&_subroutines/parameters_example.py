66b12f2426856ac68570a74e0cb2e89d32ccb76e
saffronimaging-glitch <saffronimaging@gmail.com>
2026-05-09 12:30:23 +0100

    Implement greet_player function with parameters
    Added a function to greet players by name.

---
+++ 08_functions_&_subroutines/parameters_example.py
@@ -0,0 +1,11 @@
+# PARAMETERS
+# Parameters allow us to pass information into a subroutine.
+
+def greet_player(player_name):
+    print("Hello", player_name)
+    print("Welcome to the quiz!")
+
+
+greet_player("David")
+greet_player("Aisha")
+greet_player("Luan")
